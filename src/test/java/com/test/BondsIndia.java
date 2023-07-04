3c41222caa8f4961891833750329338106fa2fdc
muralic <luckyleo1204@gmail.com>
2023-07-04 12:42:38 +0530

    update

--- src/test/java/com/test/BondsIndia.java
+++ src/test/java/com/test/BondsIndia.java
@@ -31,7 +31,7 @@ public class BondsIndia {
         driver.manage().window().maximize();
       //  Assert.assertEquals(driver.getTitle().trim().toString(), "BondsIndia");
         WebElement element=driver.findElement(By.xpath("//input[@placeholder=\"Enter your email\"]"));
-        scrolltoView(element);
+     //   scrolltoView(element);
 
     }
 
@@ -39,46 +39,48 @@ public class BondsIndia {
     public void LoginButtonOnHomePage() throws InterruptedException {
         driver.get("https://master.d1cmknsx9mlrhu.amplifyapp.com");
         driver.manage().window().maximize();
-        WebElement login=driver.findElement(By.xpath("//div/button[text()='Login']"));
-        scrolltoView(login);
+        WebElement login = driver.findElement(By.xpath("//div/button[text()='Login']"));
+        //scrolltoView(login);
         Thread.sleep(5000);
-    }
+//    }
+//
+//    public void mouseHoverJScript(WebElement HoverElement) {
+//        try {
+//            if (isElementPresent(HoverElement)) {
+//
+//                String mouseOverScript = "if(document.createEvent){var evObj = document.createEvent('MouseEvents');evObj.initEvent('mouseover',
+//
+//                true, false); arguments[0].dispatchEvent(evObj);} else if(document.createEventObject) { arguments[0].fireEvent('onmouseover');}";
+//            ((JavascriptExecutor) driver).executeScript(mouseOverScript,
+//                    HoverElement);
+//
+//        } else {
+//            System.out.println("Element was not visible to hover " + "\n");
+//
+//        }
+//    } catch (StaleElementReferenceException e) {
+//        System.out.println("Element with " + HoverElement
+//                + "is not attached to the page document"
+//                + e.getStackTrace());
+//    } catch (NoSuchElementException e) {
+//        System.out.println("Element " + HoverElement + " was not found in DOM"
+//                + e.getStackTrace());
+//    } catch (Exception e) {
+//        e.printStackTrace();
+//        System.out.println("Error occurred while hovering"
+//                + e.getStackTrace());
+//    }
+//}
+//
+//    public void scrolltoView(WebElement element){
+//        JavascriptExecutor js=((JavascriptExecutor)driver);
+//        js.executeScript("arguments[0].scrollIntoView(true);", element);
+//    }
+//
+//    @AfterMethod
+//    public void teardown(){
+//        driver.quit();
+//    }
+//}
 
-    public void mouseHoverJScript(WebElement HoverElement) {
-        try {
-            if (isElementPresent(HoverElement)) {
-
-                String mouseOverScript = "if(document.createEvent){var evObj = document.createEvent('MouseEvents');evObj.initEvent('mouseover',
-
-                true, false); arguments[0].dispatchEvent(evObj);} else if(document.createEventObject) { arguments[0].fireEvent('onmouseover');}";
-            ((JavascriptExecutor) driver).executeScript(mouseOverScript,
-                    HoverElement);
-
-        } else {
-            System.out.println("Element was not visible to hover " + "\n");
-
-        }
-    } catch (StaleElementReferenceException e) {
-        System.out.println("Element with " + HoverElement
-                + "is not attached to the page document"
-                + e.getStackTrace());
-    } catch (NoSuchElementException e) {
-        System.out.println("Element " + HoverElement + " was not found in DOM"
-                + e.getStackTrace());
-    } catch (Exception e) {
-        e.printStackTrace();
-        System.out.println("Error occurred while hovering"
-                + e.getStackTrace());
-    }
-}
-
-    public void scrolltoView(WebElement element){
-        JavascriptExecutor js=((JavascriptExecutor)driver);
-        js.executeScript("arguments[0].scrollIntoView(true);", element);
-    }
-
-    @AfterMethod
-    public void teardown(){
-        driver.quit();
-    }
-}
+    }}
